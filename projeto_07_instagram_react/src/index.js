d179e2d518ab143331c7bfcca8827197a8a2b251
vitorcarneiro <vitorcarneiro.sdo@gmail.com>
2021-12-06 01:18:40 -0300

    added Footer

--- projeto_07_instagram_react/src/index.js
+++ projeto_07_instagram_react/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import Header from "./components/Header";
 import MainContent from "./components/Main";
 import Aside from "./components/Aside";
+import Footer from "./components/Footer";
 
 function App() {
 	const userData = {name: "cebolinha", description: "O único que usa sapato"};
@@ -12,6 +13,7 @@ function App() {
 			<Header />
 			<MainContent />
 			<Aside userData={userData} />
+			<Footer />
 		</div>
 	);
 }
